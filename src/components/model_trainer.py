eb7b6d38cded468f5d04bead392255352e9d7385
IbrahimBagwan1 <irbagwan12@gmail.com>
2025-05-10 12:39:49 +0530

    model_trainer

---
+++ src/components/model_trainer.py
@@ -0,0 +1,30 @@
+import os,sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+  AdaBoostRegressor,
+  RandomForestRegressor,
+  GradientBoostingRegressor
+)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_obj
+
+@dataclass
+class ModelTrainerConfig:
+  trained_model_file_path = os.path.join('artifacts', 'model.pkl')
+
+class ModelTriner:
+  def __init__(self):
+    self.model_trainer_config = ModelTrainerConfig()
+
+  def initiate_model_trainer(self, train_array, test_array):
+    pass
