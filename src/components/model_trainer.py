88e609830c043e953e941f7d6f2e483f58ccd9b1
IbrahimBagwan1 <irbagwan12@gmail.com>
2025-05-10 13:06:58 +0530

    model

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -50,6 +50,10 @@ class ModelTriner:
       model_report:dict = evaluate_model(X_train=X_train,y_train=y_train, X_test=X_test, y_test=y_test, 
                                          models= models)
 
+      best_model_score = max(sorted(model_report.values()))
+      best_model_name = list(model_report.keys())[
+        list(model_report.values()).index(best_model_score)
+      ]
 
     except Exception as e:
       raise CustomException(e,sys)
